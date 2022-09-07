1e7a926e2b547a7fccea21967e4d0fb0676c2704
abyanfalah <abyanf410@gmail.com>
2022-09-07 07:02:30 +0700

    Update post-created.blade.php

--- resources/views/modals/post-created.blade.php
+++ resources/views/modals/post-created.blade.php
@@ -2,7 +2,6 @@
     <div class="modal-dialog modal-dialog-centered ">
         <div class="modal-content border-success">
             <div class="modal-body bg-success text-white d-flex flex-column justify-content-center align-items-center">
-
                 Post created successfully
                 <button class="btn btn-light text-success" data-bs-dismiss="modal">OK</button>
             </div>
