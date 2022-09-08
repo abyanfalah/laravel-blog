523803adc73b0d43e82c39743b51f3546c33c3b0
abyanfalah <abyanf410@gmail.com>
2022-09-08 19:43:35 +0700

    add success-modal.blade.php

---
+++ resources/views/modals/success-modal.blade.php
@@ -0,0 +1,14 @@
+<div class="modal" id="successModal">
+    <div class="modal-dialog modal-dialog-centered ">
+        <div class="modal-content border-success">
+            <div class="modal-body bg-success text-white d-flex flex-column justify-content-center align-items-center">
+                <i class="bi-check-circle-fill display-1"></i>
+                <p class="fw-bold">Post <span class="d-inline" id="operationName"></span> successfully</p>
+                <button class="btn btn-light text-success" data-bs-dismiss="modal">OK</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
