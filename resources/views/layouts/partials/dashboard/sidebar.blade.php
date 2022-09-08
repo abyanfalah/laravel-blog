a9b95ceb31a5d0124f6ac54c6377b9d82f07491f
abyanfalah <abyanf410@gmail.com>
2022-09-08 19:46:27 +0700

    Update sidebar.blade.php fix user dropdown

--- resources/views/layouts/partials/dashboard/sidebar.blade.php
+++ resources/views/layouts/partials/dashboard/sidebar.blade.php
@@ -26,20 +26,23 @@
 
 
     <hr>
-    <div class="nav-pills">
-        <div class="nav-item">
-            <div class="dropdown">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown link
-                </a>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        </div>
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center link-light text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="/assets/img/authors/{{ auth()->user()->id }}.jpg" alt="{{ auth()->user()->name }}" width="32" height="32" class="rounded-circle me-2">
+        <strong class="text-white">{{ auth()->user()->name }}</strong>
+      </a>
+      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+        <li><a class="dropdown-item" href="/dashboard/posts/create">New post</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li>
+            <form action="/logout" method="POST">
+                @csrf
+                <button class="dropdown-item">Logout</button>
+            </form>
+        </li>
+      </ul>
     </div>
 
   </div>
