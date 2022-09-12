9208c5f6e4bb0c59f60b0e8cb373b5c3fa32fc41
abyanfalah <abyanf410@gmail.com>
2022-09-12 11:30:06 +0700

    add delete.blade.php

---
+++ resources/views/modals/delete.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="deleteItemModal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-danger text-white fw-bold">
+                Delete post
+            </div>
+            <div class="modal-body">
+                Are you sure you want to delete this item?
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary" data-bs-dismiss="modal">No</button>
+                <form action="/dashboard/posts/" method="post">
+                    @method("delete")
+                    @csrf
+                    <button class="btn btn-outline-danger">Yes</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
