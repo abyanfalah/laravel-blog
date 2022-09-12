c01b1b3b7c0a86483439e1403b8b0a350195f906
abyanfalah <abyanf410@gmail.com>
2022-09-12 11:29:53 +0700

    Update success-modal.blade.php

--- resources/views/modals/success-modal.blade.php
+++ resources/views/modals/success-modal.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content border-success">
             <div class="modal-body bg-success text-white d-flex flex-column justify-content-center align-items-center">
                 <i class="bi-check-circle-fill display-1"></i>
-                <p class="fw-bold">Post <span class="d-inline" id="operationName"></span> successfully</p>
+                <p class="fw-bold">Item <span class="d-inline" id="operationName"></span> successfully</p>
                 <button class="btn btn-light text-success" data-bs-dismiss="modal">OK</button>
             </div>
         </div>
