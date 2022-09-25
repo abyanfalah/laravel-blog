bea24ec14e79d5d3ea795a7e0d0991c6ef1ad5e8
abyanfalah <abyanf410@gmail.com>
2022-09-26 00:14:29 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,11 @@ class User extends Authenticatable
 
     public function posts()
     {
-        return $this->hasMany(Posts::class);
+        return $this->hasMany(Post::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return "username";
     }
 }
